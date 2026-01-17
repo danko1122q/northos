dcbd143599f3baf3a5dd623b60e91798f1580336
danko12 <davaniko1122@gmail.com>
2026-01-17 18:43:14 +0700

    Refactor icons.h with improved comments and structure

--- include/icons.h
+++ include/icons.h
@@ -1,18 +1,28 @@
 #ifndef ICONS_H
 #define ICONS_H
 
-#define ICON_SIZE 30
-#define ICON_NUMBER 5
+/* --- Icon Dimensions & Capacity --- */
+#define ICON_SIZE   30   // Each icon is a 30x30 pixel square
+#define ICON_NUMBER 5    // Total number of icons defined in the data array
 
-#define ICON_CLOSE 0
-#define ICON_MINIMIZE 1
-#define ICON_WINDOWS 2
-#define ICON_FOLDER 3
-#define ICON_CHESS 4
+/* --- Icon Index Mapping --- */
+// These constants serve as indices into the 'icons_data' 2D array.
+#define ICON_CLOSE    0  // Close button (usually an 'X')
+#define ICON_MINIMIZE 1  // Minimize button (usually a '_')
+#define ICON_WINDOWS  2  // Logo or start menu icon
+#define ICON_FOLDER   3  // File system directory icon
+#define ICON_CHESS    4  // Specific application icon (Chess game)
 
-// Ganti dari 0xFF000000 menjadi 0x00000000
+/* --- Color Definitions --- */
+// Defines the Alpha/Color value used for transparency.
+// 0x00000000 represents fully transparent pixels in an ARGB8888 format,
+// where the high byte (Alpha channel) is 0x00.
 #define ICON_TRANSPARENT 0x00000000
 
+/* --- Global Data Buffer --- */
+// 'extern' indicates that the actual pixel data is defined in another file 
+// (likely icons.c). It is a 2D array where icons_data[index] returns 
+// an array of 900 (30*30) unsigned integers (colors).
 extern unsigned int icons_data[ICON_NUMBER][ICON_SIZE * ICON_SIZE];
 
 #endif
